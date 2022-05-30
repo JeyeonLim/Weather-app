96cfed25c4b66f7790318f1c22ee0f7cb8bc3d14
JeyeonLim <jeyeonlim@gmail.com>
2022-05-30 11:25:01 +0200

    added the search engine

--- src/app.js
+++ src/app.js
@@ -42,9 +42,18 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
 }
+function search(city) {
+  let apiKey = "445c7163cebd4264ef3f8436bcb8200c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function citySubmit(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-input");
+  search(cityElement.value);
+}
 
-let city = "new york";
-let apiKey = "445c7163cebd4264ef3f8436bcb8200c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+search("chicago");
 
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", citySubmit);
